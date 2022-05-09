36a647b3b83794f09afb441f1db745aaa9220a6d
AAlessandra Keeler <askeeler@isp02.tacc.utexas.edu>
2022-05-09 17:30:40 -0500

    more routes

--- src/app.py
+++ src/app.py
@@ -20,18 +20,18 @@ def load_data():
     logging.info('Reading in fuel consumption data.')
 
     global data
-    global col
 
     df = pd.read_csv('MY2022 Fuel Consumption Ratings.csv')
+    columns = [c.replace(' ', '_').lower() for c in list(df.columns)]
+    col = [columns[i] for i in range(1, len(columns))]
+    df.columns = columns
 
-    col = df.drop(['Make', 'Model'], axis = 1).columns
-
-    for make in df['Make'].unique(): 
-        out = { make: df.loc[df['Make'] == make].groupby('Model')[col].last().to_dict(orient='index')}
+    for make in df['make'].unique(): 
+        out = { make: df.loc[df['make'] == make].groupby('model')[col].last().to_dict(orient='index')}
         if make == 'Acura' : 
-            data = {'Make': out}
+            data = {'make': out}
         else:
-            data['Make'][make] = out[make]
+            data['make'][make] = out[make]
 
     return f'Data loaded from file to dictionary.\n'
 
@@ -39,7 +39,6 @@ def load_data():
 @app.route('/print_data', methods = ['GET'])
 def print_data():
     json_obj = json.dumps(data, indent = 4)
-    
     return json_obj
 
 
@@ -48,24 +47,24 @@ def get_makes():
     """
     Outputs all makes in dataset.
     """
-    make_list = [make for make in data['Make']]
+    make_list = [make for make in data['make']]
     return jsonify(make_list)
 
-@app.route('/arguments', methods = ['GET'])
-def get_arguments():
+@app.route('/<make>/<model>/features', methods = ['GET'])
+def get_arguments(make, model):
     """
-    Ouputs all possible arguments for each make
+    Ouputs all possible features for each make
     """
-    return jsonify(col)
+    return jsonify(list(data['make'][make][model].keys()))
 
 @app.route('/<make>', methods=['GET'])
 def models_for_make(make):
     """
-    Outputs all models under a specified make. Make must be capitalized. 
+    Outputs all models under a specified make. make must be capitalized. 
 
     """
 
-    make_dict = data['Make'][make]
+    make_dict = data['make'][make]
     model_list = []
     for model in make_dict:
         model_list.append(model)
@@ -77,7 +76,14 @@ def model_data(make, model):
     """
     Gets all data for a specified make and model
     """
-    return data['Make'][make][model]
+    return data['make'][make][model]
+
+@app.route('/<make>/<model>/<feature>')
+def get_feature(make, model, feature):
+    """
+    Gets a feature for a certain make and model
+    """
+    return f"{feature} for the {model} model of {make} is {data['make'][make][model][feature]}"
 
 
 @app.route('/average_fuel_consumption_<make>/<type>/<units>')
@@ -87,8 +93,8 @@ def avg_make_consumption(make, type, units):
 
     Args:
         make (string) : the make of the vehicle (must be capitalized)
-        type (string) : Hwy, City, or Comb (hwy = highway driving, city = city driving, comb = combined driving)
-        units (string) : L or mpg (L/100km or miles per gallon)
+        type (string) : hwy, city, or comb (hwy = highway driving, city = city driving, comb = combined driving)
+        units (string) : L or mpg (L/100km or miles per gallon). Note MPG only supports has comb. 
 
     Returns: 
         String describing the average fuel consumption 
@@ -96,17 +102,46 @@ def avg_make_consumption(make, type, units):
     """
 
     if units == 'mpg':
-        index_string = "Fuel Consumption(Comb (mpg))"
+        index_string = "fuel_consumption(comb_(mpg))"
     else:
-        index_string = f"Fuel Consumption({type} (L/100 km))"
+        index_string = f"fuel_consumption({type}_(l/100_km))"
 
     fuel = []
-    for m in data['Make'][make]:
-        fuel.append(data['Make']['Acura'][m][index_string])
+    for m in data['make'][make]:
+        fuel.append(data['make'][make][m][index_string])
         
     avg = s.mean(fuel)
 
     return f"The average {index_string} for {make} is {avg} "
+
+
+@app.route('/<make>/average_<feature>')
+def avg_feature(make, feature):
+    """ 
+    Gets the average of any numerical feature
+    """
+    non_numerical = ["vehicle_class", "transmission", "fuel_type"]
+    if feature in non_numerical:
+        return "This is a non numerical feature and not able to be averaged "
+
+    feature_sum = []
+    for m in data['make'][make]:
+        feature_sum.append(data['make'][make][m][feature])
+    avg = s.mean(feature_sum)
+
+    return f"The average {feature} for {make} is {avg}"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
