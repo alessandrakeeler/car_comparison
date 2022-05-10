039df4cd70619bc9aa617d239f1964ce991c5ec4
AAlessandra Keeler <askeeler@isp02.tacc.utexas.edu>
2022-05-10 13:46:54 -0500

    app done and scatter done

--- src/app.py
+++ src/app.py
@@ -2,23 +2,24 @@ from flask import Flask, jsonify, request
 import logging
 import xmltodict
 import pandas as pd
-import json 
-import statistics as s 
+import json
+import statistics as s
 import os
 import redis
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 """ redis_ip = os.environ.get('REDIS_IP')
 if not redis_ip:
     raise Exception() """
 
-rd=redis.StrictRedis(host= '127.0.0.1', port=6413, db=13)
+rd = redis.StrictRedis(host="127.0.0.1", port=6413, db=13)
 
 
 app = Flask(__name__)
 
-data = {}
 
-@app.route('/data', methods=['POST', 'GET'])
+@app.route("/data", methods=["POST", "GET"])
 def load_data():
     """
     Loads fuel consumption data.
@@ -26,53 +27,58 @@ def load_data():
     Returns:
         string that states that the data has been loaded.
     """
-    logging.info('Reading in fuel consumption data.')
+    logging.info("Reading in fuel consumption data.")
 
     global data
 
-    if request.method == 'POST':
+    if request.method == "POST":
         rd.flushdb()
 
-        df = pd.read_csv('MY2022 Fuel Consumption Ratings.csv')
-        columns = [c.replace(' ', '_').lower() for c in list(df.columns)]
+        df = pd.read_csv("MY2022 Fuel Consumption Ratings.csv")
+        columns = [c.replace(" ", "_").lower() for c in list(df.columns)]
         col = [columns[i] for i in range(1, len(columns))]
         df.columns = columns
 
-        for make in df['make'].unique(): 
-            out = { make: df.loc[df['make'] == make].groupby('model')[col].last().to_dict(orient='index')}
-            if make == 'Acura' : 
-                data = {'make': out}
+        for make in df["make"].unique():
+            out = {
+                make: df.loc[df["make"] == make]
+                .groupby("model")[col]
+                .last()
+                .to_dict(orient="index")
+            }
+            if make == "Acura":
+                data = {"make": out}
             else:
-                data['make'][make] = out[make]
-        final_data = data['make']
+                data["make"][make] = out[make]
+        final_data = data["make"]
 
         for make in final_data:
             rd.set(make, json.dumps(final_data[make]))
 
         return "Data loaded into redis"
-        
-    elif request.method == 'GET':
+
+    elif request.method == "GET":
         makes = []
         for item in rd.keys():
-            makes.append({item.decode('utf-8'): json.loads(rd.get(item) ) })
-        return json.dumps(makes, indent = 4)
+            makes.append({item.decode("utf-8"): json.loads(rd.get(item))})
+        return json.dumps(makes, indent=4)
 
     else:
         return "Method only supports POST and GET"
 
-    
 
-@app.route('/makes', methods = ['GET'])
+@app.route("/makes", methods=["GET"])
 def get_makes():
     """
     Outputs all makes in dataset.
     """
     make_list = []
     for key in rd.keys():
-        make_list.append(key.decode('utf-8'))
+        make_list.append(key.decode("utf-8"))
     return jsonify(make_list)
 
-@app.route('/<make>/<model>/features', methods = ['GET'])
+
+@app.route("/<make>/<model>/features", methods=["GET"])
 def get_arguments(make, model):
     """
     Ouputs all possible features for each make
@@ -80,10 +86,11 @@ def get_arguments(make, model):
     feature_list = list(json.loads(rd.get(make))[model].keys())
     return jsonify(feature_list)
 
-@app.route('/<make>', methods=['GET'])
+
+@app.route("/<make>", methods=["GET"])
 def models_for_make(make):
     """
-    Outputs all models under a specified make. 
+    Outputs all models under a specified make.
 
     """
     make_dict = json.loads(rd.get(make))
@@ -93,7 +100,8 @@ def models_for_make(make):
 
     return jsonify(model_list)
 
-@app.route('/<make>/<model>/data', methods = ['GET'])
+
+@app.route("/<make>/<model>/data", methods=["GET"])
 def model_data(make, model):
     """
     Gets all data for a specified make and model
@@ -101,8 +109,7 @@ def model_data(make, model):
     return json.loads(rd.get(make))[model]
 
 
-
-@app.route('/<make>/<model>/<feature>')
+@app.route("/<make>/<model>/<feature>")
 def get_feature(make, model, feature):
     """
     Gets a feature for a certain make and model
@@ -110,8 +117,7 @@ def get_feature(make, model, feature):
     return f"{feature} for the {model} model of {make} is {json.loads(rd.get(make))[model][feature]}"
 
 
-
-@app.route('/average_fuel_consumption_<make>/<type>/<units>', methods = ['GET'])
+@app.route("/average_fuel_consumption_<make>/<type>/<units>", methods=["GET"])
 def avg_make_consumption(make, type, units):
     """
     Gets the average fuel consumption of a make of vehicle
@@ -119,14 +125,14 @@ def avg_make_consumption(make, type, units):
     Args:
         make (string) : the make of the vehicle (must be capitalized)
         type (string) : hwy, city, or comb (hwy = highway driving, city = city driving, comb = combined driving)
-        units (string) : L or mpg (L/100km or miles per gallon). Note MPG only supports has comb. 
+        units (string) : L or mpg (L/100km or miles per gallon). Note MPG only supports has comb.
 
-    Returns: 
-        String describing the average fuel consumption 
-  
+    Returns:
+        String describing the average fuel consumption
+
     """
 
-    if units == 'mpg':
+    if units == "mpg":
         index_string = "fuel_consumption(comb_(mpg))"
     else:
         index_string = f"fuel_consumption({type}_(l/100_km))"
@@ -135,15 +141,15 @@ def avg_make_consumption(make, type, units):
     fuel = []
     for m in make_dict:
         fuel.append(make_dict[m][index_string])
-        
+
     avg = s.mean(fuel)
 
     return f"The average {index_string} for {make} is {avg} "
 
 
-@app.route('/<make>/average_<feature>', methods = ['GET'])
+@app.route("/<make>/average_<feature>", methods=["GET"])
 def avg_feature(make, feature):
-    """ 
+    """
     Gets the average of any numerical feature
     """
     non_numerical = ["vehicle_class", "transmission", "fuel_type"]
@@ -159,16 +165,96 @@ def avg_feature(make, feature):
     return f"The average {feature} for {make} is {avg}"
 
 
-#@app.route('/scatter/<feature1>/<feature2>/<comparison>')
-#def scatter_feature(feature1, feature2, comparison):
+@app.route("/scatter/<feature1>/<feature2>", methods=["GET"])
+def scatter_feature_make(feature1, feature2):
     """
-    Scatterplots two features against eachother, compared on a comparison value. (Can be numerical or categorical)
-    Ex) smog_rating as feature one vs fuel consumption (will default to mpg) for feature2, compared on cylinder type. 
-    Ex) fuel_consumption vs co2 rating compared on make 
-
+    Scatterplots two features against eachother, compared on make
     """
 
-@app.route('/interact', methods=['GET'])
+    features = [
+        "make",
+        "model",
+        "vehicle_class",
+        "engine_size(l)",
+        "cylinders",
+        "transmission",
+        "fuel_type",
+        "fuel_consumption_(city_(l/100_km)",
+        "fuel_consumption(hwy_(l/100_km))",
+        "fuel_consumption(comb_(l/100_km))",
+        "fuel_consumption(comb_(mpg))",
+        "co2_emissions(g/km)",
+        "co2_rating",
+        "smog_rating",
+    ]
+
+    ## TODO figure out how to make less horrifically inneficient
+    df = pd.DataFrame(columns=features)
+    for key in rd.keys():
+        make_dict = json.loads(rd.get(key))
+        for model in make_dict:
+            list_items = list((make_dict[model]).items())
+            data_list = [item[1] for item in list_items]
+            df = df.append(pd.Series(data_list, index=df.columns), ignore_index=True)
+
+    sns.set(rc={"figure.figsize": (11.7, 8.27)})
+    scatter = sns.scatterplot(data=df, x=feature1, y=feature2, hue="make")
+
+    fig = scatter.get_figure()
+    plt.legend(fontsize="small", loc=2, bbox_to_anchor=(1, 1.16))
+    fig.savefig(f"{feature1}_vs_{feature2}.png")
+    return "Graph saved \n"
+
+
+@app.route("/scatter/<feature1>/<feature2>/<comparison>", methods=["GET"])
+def scatter_feature_comparison(feature1, feature2, comparison):
+    """
+    Scatterplots two numerical features, compared on a custom categorical feature
+    """
+    features = [
+        "make",
+        "model",
+        "vehicle_class",
+        "engine_size(l)",
+        "cylinders",
+        "transmission",
+        "fuel_type",
+        "fuel_consumption_(city_(l/100_km)",
+        "fuel_consumption(hwy_(l/100_km))",
+        "fuel_consumption(comb_(l/100_km))",
+        "fuel_consumption(comb_(mpg))",
+        "co2_emissions(g/km)",
+        "co2_rating",
+        "smog_rating",
+    ]
+
+    categorical_features = [
+        "make",
+        "model",
+        "vehicle_class",
+        "transmission",
+        "fuel_type",
+    ]
+    if comparison not in categorical_features:
+        return "Sorry, this is a numerical feature! Please pick a categorical feature as the comparison factor \n"
+
+    df = pd.DataFrame(columns=features)
+    for key in rd.keys():
+        make_dict = json.loads(rd.get(key))
+        for model in make_dict:
+            list_items = list((make_dict[model]).items())
+            data_list = [item[1] for item in list_items]
+            df = df.append(pd.Series(data_list, index=df.columns), ignore_index=True)
+
+    sns.set(rc={"figure.figsize": (11.7, 8.27)})
+    scatter = sns.scatterplot(data=df, x=feature1, y=feature2, hue=comparison)
+    fig = scatter.get_figure()
+    plt.legend(fontsize="small", loc=2)
+    fig.savefig(f"{feature1}_vs_{feature2}_compared_on_{comparison}.png")
+    return "Graph saved \n"
+
+
+@app.route("/interact", methods=["GET"])
 def interact():
     """
     Outputs information on how to interact with the application.
@@ -176,12 +262,13 @@ def interact():
     Returns:
         ret (string): how to interact with the application
     """
-    logging.info('How to interact with application')
+    logging.info("How to interact with application")
 
     ret = "How to interact with the application:\n"
-    ret+= "curl localhost:<port number>/interact                           (GET) prints this infromation\n"
-    ret+= "curl localhost:<port number>/load_data -X POST                  (POST) reads in the fuel consumption data\n"
-    return ret;
+    ret += "curl localhost:<port number>/interact                           (GET) prints this infromation\n"
+    ret += "curl localhost:<port number>/load_data -X POST                  (POST) reads in the fuel consumption data\n"
+    return ret
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
