e5eea897a610278788c8f6a1d448665aa464c6cc
Sydney Loats <sydneyloats@gmail.com>
2022-05-12 02:22:19 -0500

    test file

--- test/test_flask.py
+++ test/test_flask.py
@@ -24,6 +24,14 @@ def test_data_upload():
     assert response.status_code == 200
     assert response.content == b'Data loaded into redis \n'
 
+def teset_jobs_info():
+    route = f'{api_prefix}/jobs'
+    response = requests.get(route)
+
+    assert response.ok == True
+    assert response.status_code == 200
+    assert bool(re.search('To submit a job,', response.text)) == True
+
 load_data()
 
 def test_interact():
