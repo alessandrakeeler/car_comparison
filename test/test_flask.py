98be79a4a872c7325c303ef662e440eab4d7cc08
Sydney Loats <sydneyloats@gmail.com>
2022-05-12 02:04:36 -0500

    currently writing test file

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,6 +1,28 @@
 from app import*
 
 import pytest
+import requests
+
+api_host = 'localhost'
+api_port = '5016'
+api_prefix = f'http://{api_host}:{api_port}'
+
+
+def test_info():
+    route = f'{api_prefix}/'
+    response = requests.get(route)
+
+    assert response.ok == True
+    assert response.status_code == 200
+    assert bool(re.search('Try the following routes', response.text)) == True
+
+def test_data_upload():
+    route = f'{api_prefix}/data'
+    response = requests.get(route)
+
+    assert response.ok == True
+    assert response.status_code == 200
+    assert response.content == b'Data loaded into redis \n'
 
 load_data()
 
@@ -18,3 +40,4 @@ def test_avg_make_consumption():
 
 def test_avg_feature():
     assert isinstance(avg_feature('a', 'b'), str) == True
+
